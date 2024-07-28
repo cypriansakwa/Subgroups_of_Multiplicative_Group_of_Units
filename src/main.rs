aa3735ae17ebaf13161846215354889fa1774237
cypriansakwa <cypriansakwa@gmail.com>
2024-07-28 19:05:26 +0300

    old file

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ fn generate_all_subgroups(n: u64) -> Vec<HashSet<u64>> {
 }
 
 fn main() {
-    let n = 8; // Replace with the desired value of n
+    let n = 34; // Replace with the desired value of n
     let subgroups = generate_all_subgroups(n);
     println!("Subgroups of Z_{}^*:", n);
     for subgroup in subgroups {
